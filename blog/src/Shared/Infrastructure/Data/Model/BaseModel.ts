af050f6e94eabfa2a3122714fc0f3a8fa083cf99
Adriein <adria.claret@gmail.com>
2022-05-26 20:18:14 +0200

    fixed timestamp precision

--- blog/src/Shared/Infrastructure/Data/Model/BaseModel.ts
+++ blog/src/Shared/Infrastructure/Data/Model/BaseModel.ts
@@ -4,9 +4,9 @@ export abstract class BaseModel {
   @PrimaryColumn({name: 'id', type: 'varchar'})
   public id!: string
 
-  @CreateDateColumn({name: 'created_at'})
+  @CreateDateColumn({name: 'created_at', type: 'timestamp', precision: 0})
   public createdAt!: Date
 
-  @UpdateDateColumn({name: 'updated_at'})
+  @UpdateDateColumn({name: 'updated_at', type: 'timestamp', precision: 0})
   public updatedAt!: Date
 }
